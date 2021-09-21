0232cf4945eff869b640c5800ee350d1482b81d6
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2021-09-20 22:21:17 -0300

    Initial commit

---
+++ src/main/java/com/marcosoft/soaplab/SoapLabApplication.java
@@ -0,0 +1,13 @@
+package com.marcosoft.soaplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosoft/soaplab/SoapLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosoft.soaplab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
